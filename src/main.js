fe052888897da917fdce2b84c83ec930756dbe91
Guest User <Guest@Epicodus-6D.local>
2018-03-14 13:37:45 -0700

    properly separates logic into main.js and rpg.js

--- src/main.js
+++ src/main.js
@@ -1,34 +1,19 @@
 import $ from 'jquery';
 import 'bootstrap';
 import './styles.css';
+import { assignAttrPoints } from './../src/rpg.js';
+
 
 $(document).ready(function() {
 
+
+
+
   $('.modifier').click(function() {
     var thisId = event.target.id;
-    var valNumber = thisId.replace(/-(.*?)$/, 'Val');
-    console.log(valNumber);
-    var attribute = thisId.replace(/^(.*?)-/, '');
-    console.log(attribute);
     var attributePoints = parseInt($('.attribute-points').text());
-    var attributeVal = parseInt($('#'+valNumber).text());
-    if (attributePoints == 0) {
-      alert("You have no more attribute points!");
-    }
-    if (attribute == "increment") {
-      if (attributePoints > 0) {
-        $('#'+valNumber).html(attributeVal+1);
-        $('.attribute-points').html(attributePoints-1);
-      }
-    } else {
-      if (attributeVal != 0) {
-        if (attributeVal > 0) {
-          $('#'+valNumber).html(attributeVal-1);
-          $('.attribute-points').html(attributePoints+1);
-        }
-        $('#'+valNumber).html(attributeVal-1);
-      }
-    }
+    // alert(thisId);
+    assignAttrPoints(thisId, attributePoints);
   });
   //
   // $('#char-creation').submit(function(event) {
--- src/rpg.js
+++ src/rpg.js
@@ -1,7 +1,31 @@
-var attrStats = [];
-export function assignAttrPoints () {
-  $('#char-creation :input').each(function() {
-    var input = $(this).val();
-    attrStats.push(parseInt(input));
-  });
+
+
+export function assignAttrPoints (thisId, attributePoints) {
+
+  var valNumber = thisId.replace(/-(.*?)$/, 'Val');
+  // console.log(valNumber);
+  var attribute = thisId.replace(/^(.*?)-/, '');
+  // console.log(attribute);
+
+  var attributeVal = parseInt($('#'+valNumber).text());
+
+
+  if (attributePoints == 0) {
+    alert("You have no more attribute points!");
+  }
+  if (attribute == "increment") {
+    if (attributePoints > 0) {
+      $('#'+valNumber).html(attributeVal+1);
+      $('.attribute-points').html(attributePoints-1);
+    }
+  } else {
+    if (attributeVal != 0) {
+      if (attributeVal > 0) {
+        $('#'+valNumber).html(attributeVal-1);
+        $('.attribute-points').html(attributePoints+1);
+      }
+      $('#'+valNumber).html(attributeVal-1);
+    }
+  }
+  return attributePoints;
 }
